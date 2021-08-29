6f10f0d812d6e4adc70449509f42b93c87825387
Fernando Herrera <klerith@yahoo.com>
2021-08-29 15:07:35 -0400

    tabs creados

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    TabsComponent
   ],
   imports: [
     CommonModule
